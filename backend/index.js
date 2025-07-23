80c7b04b858123e6319cbb96227f1ca9fdf814cf
nihala shirin <nihalashirin02@gmail.com>
2025-07-23 12:31:39 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,24 @@
 import express from "express";
-import userRoutes from "../routes/userRoutes.js";       
-import { connectDB } from "../config/connectDB.js";      
-import taskRoutes from "../routes/taskRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+import { connectDB } from "./config/connectDB.js";
 import cors from "cors";
-import { createServerlessHandler } from "vercel-express"; 
 
 const app = express();
+
 
 connectDB();
 
 app.use(cors({
-    origin: 'https://task-mangement-ashy.vercel.app',
-    credentials: true
+  origin: 'https://task-mangement-ashy.vercel.app',
+  credentials: true
 }));
 
 app.use(express.json());
 
+
 app.use('/user', userRoutes);
 app.use('/task', taskRoutes);
 
-export default createServerlessHandler(app); 
+
+export default app;
