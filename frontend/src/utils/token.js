131878e4a7563126879acaff0b7ff3ec8c9213d0
nihala shirin <nihalashirin02@gmail.com>
2025-07-23 12:11:58 +0530

    backend code added on this

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -1,4 +1,4 @@
-export const BASE_URL=' http://localhost:5431/';
+export const BASE_URL='https://task-mangement-okny.vercel.app/';
 export const ACCESS_TOKEN='access_token';
 export const REFRESH_TOKEN='refresh_token';
 export const USERNAME='username';
