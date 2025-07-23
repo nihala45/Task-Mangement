9320fd440642f9813d5ff2a4c98fc1a414ed6be3
nihala shirin <nihalashirin02@gmail.com>
2025-07-23 12:08:00 +0530

    completed backend

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,22 @@
-import express from "express"
+import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import { connectDB } from "./config/connectDB.js";
-import taskRoutes from "./routes/taskRoutes.js"
-import cors from 'cors'
-const app = express();
-const port = 5431;
+import taskRoutes from "./routes/taskRoutes.js";
+import cors from "cors";
+import { createServerlessHandler } from "vercel-express"; 
 
-app.use(express.json())
-connectDB()
+const app = express();
+
+connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://task-mangement-ashy.vercel.app',
     credentials: true
 }));
 
-app.use('/user',userRoutes)
-app.use('/task',taskRoutes)
+app.use(express.json());
 
+app.use('/user', userRoutes);
+app.use('/task', taskRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
+export default createServerlessHandler(app);
