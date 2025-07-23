2f0028bafb3a048675b4d8d8ceff3b83a1969100
nihala shirin <nihalashirin02@gmail.com>
2025-07-23 12:58:45 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import { connectDB } from "./config/connectDB.js";
-import cors from "cors";
 
 const app = express();
 
 
 connectDB();
 
+
 app.use(cors({
   origin: 'https://task-mangement-ashy.vercel.app',
-  credentials: true
+  credentials: true,
 }));
 
 app.use(express.json());
@@ -19,6 +23,5 @@ app.use(express.json());
 
 app.use('/user', userRoutes);
 app.use('/task', taskRoutes);
-
 
 export default app;
