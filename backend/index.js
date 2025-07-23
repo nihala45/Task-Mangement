541df3a79e9793ce29590c562730bf492e215dbb
nihala shirin <nihalashirin02@gmail.com>
2025-07-23 13:20:12 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,29 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
-import { connectDB } from "./config/connectDB.js";
 
 const app = express();
-
-
-connectDB();
-
 
 const allowedOrigins = [
   'https://task-mangement-ashy.vercel.app',
   'https://task-mangement-git-main-nihala45s-projects.vercel.app'
 ];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 
-
-app.use('/user', userRoutes);
-app.use('/task', taskRoutes);
+app.use("/user", userRoutes);
+app.use("/task", taskRoutes);
 
 export default app;
